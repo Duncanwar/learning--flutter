00bb1ff5815fee45bb16ac5e46ec766b7a36e12a
Duncanwar <semunda9@gmail.com>
2022-02-10 00:01:57 +0200

    completed

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: HomePage(), theme: ThemeData(primarySwatch: Colors.purple)));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Awesome App"),
+        ),
+        body: Container(child: Text("HI Flutter!")));
+  }
+}
